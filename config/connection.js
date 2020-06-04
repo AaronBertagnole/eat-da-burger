00a5fde03e66e24dab7269c4635dfbc67acbb54f
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-06-03 20:15:56 -0600

    added jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection; 
+if(process.env.JAWSDB_URL){
+
+  } else{
+   mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "burgers_db"
 });
-
+  };
 // Make connection.
 connection.connect(function(err) {
   if (err) {
